57abc416439f0cfeedf06a51e48d5882a77f0971
ma c <2694969783@qq.com>
2016-11-08 19:37:40 +0800

    商家认证、实名认证

---
+++ GouGou-Live/Classes/Me/Views/CertificateView/IdentityIfonView.h
@@ -0,0 +1,13 @@
+//
+//  IdentityIfonView.h
+//  GouGou-Live
+//
+//  Created by ma c on 16/11/8.
+//  Copyright © 2016年 LXq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IdentityIfonView : UIView
+
+@end
---
+++ GouGou-Live/Classes/Me/Views/CertificateView/IdentityIfonView.m
@@ -0,0 +1,21 @@
+//
+//  IdentityIfonView.m
+//  GouGou-Live
+//
+//  Created by ma c on 16/11/8.
+//  Copyright © 2016年 LXq. All rights reserved.
+//
+
+#import "IdentityIfonView.h"
+
+@implementation IdentityIfonView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

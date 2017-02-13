f635c86b176b99d2d81c54ff85117d4ba360bb61
NewMe <1937815837@qq.com>
2017-02-13 19:30:18 +0800

    二

--- GouGou-Live/Classes/LiveAction/Controllers/Service/ServiceViewController.m
+++ GouGou-Live/Classes/LiveAction/Controllers/Service/ServiceViewController.m
@@ -14,7 +14,7 @@
 // 表情
 #define EMOJI_CODE_TO_SYMBOL(x) ((((0x808080F0 | (x & 0x3F000) >> 4) | (x & 0xFC0) << 10) | (x & 0x1C0000) << 18) | (x & 0x3F) << 24);
 
-@interface ServiceViewController ()<UITextFieldDelegate, EaseMessageViewControllerDelegate>
+@interface ServiceViewController ()<UITextFieldDelegate, EaseMessageViewControllerDelegate, EaseMessageViewControllerDataSource>
 
 @property(nonatomic, strong) TalkingView *talkView; /**< 聊天输入框 */
 
@@ -84,7 +84,7 @@
     
     [[EaseBaseMessageCell appearance] setAvatarSize:44.f];//设置头像大小
     //    [[EaseBaseMessageCell appearance] setAvatarCornerRadius:20.f];//设置头像圆角
-    
+    self.dataSource = self;
     
     // 隐藏输入框 自定义输入框
     self.chatToolbar.hidden = YES;
